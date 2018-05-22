785147308ffaeaded99973e0679019b1f4fdbea4
shankupinku41 <39461223+shankupinku41@users.noreply.github.com>
2018-05-22 14:24:14 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 /**
  * Main AngularJS Web Application
  */
+// Initialisation of the angular module
 var app = angular.module('angularapplication', [
     'ngRoute'
 ]);
@@ -9,26 +10,29 @@ var app = angular.module('angularapplication', [
  * Configure the Routes
  */
 app.config(['$routeProvider', function($routeProvider) {
-								$routeProvider
-								.when("/", {
-										templateUrl: "home.html",
-										controller: "" })
-                                .when("/addContact", {
-                                        templateUrl: "partials/addcontact.html",
-                                        controller: "addContactCtrl" })
-                                .when("/listContacts", {
-                                         templateUrl: "partials/listcontacts.html",
-                                         controller: "listContactCtrl" })
-                                         
-										 
-								.when("/contactMe", {
-                                         templateUrl: "partials/contactme.html",
-                                         controller: "contact_me" })		
-								
-										// else 404
-								.otherwise("/404", {
-										templateUrl: "partials/404.html",
-										controller: ""});
-								}]);
+    $routeProvider
+        .when("/", {
+            templateUrl: "home.html",
+            controller: ""
+        })
+        .when("/addContact", {
+            templateUrl: "partials/addcontact.html",
+            controller: "addContactCtrl"
+        })
+        .when("/listContacts", {
+            templateUrl: "partials/listcontacts.html",
+            controller: "listContactCtrl"
+        })
 
 
+        .when("/contactMe", {
+            templateUrl: "partials/contactme.html",
+            controller: "contact_me"
+        })
+
+        // else 404
+        .otherwise("/404", {
+            templateUrl: "partials/404.html",
+            controller: ""
+        });
+}]);
