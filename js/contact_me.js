d032ef706b7abbf298fa9a860eb6c90d404fe8e8
shankupinku41 <39461223+shankupinku41@users.noreply.github.com>
2018-05-22 14:22:50 +0530

    Update contact_me.js

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,9 +1,6 @@
-app.controller('contact_me', ['$scope', '$rootScope', function($scope,$rootScope) {
-	$scope.name = "ANURAG GHOSH";
-	$scope.mobilenumber = "9945859687";
-	$scope.email = "anuraggho@gmail.com";
-	$scope.statuscandidate = "Active";
-
-	
-	
-}]); 
+app.controller('contact_me', ['$scope', '$rootScope', function($scope, $rootScope) {
+    $scope.name = "ANURAG GHOSH";
+    $scope.mobilenumber = "9945859687";
+    $scope.email = "anuraggho@gmail.com";
+    $scope.statuscandidate = "Active";
+}]);
